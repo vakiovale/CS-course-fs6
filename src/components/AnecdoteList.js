f58d7e6c11ce6ff87b7466c135de4940901b128b
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-03-11 15:26:32 +0200

    Using Provider and using connect in rest of the components

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { anecdoteVoter } from '../reducers/anecdoteReducer'
 import { notificationAdder, notificationResetter } from '../reducers/notificationReducer'
 
 class AnecdoteList extends React.Component {
   render() {
-    const anecdotes = this.props.store.getState().anecdotes
+    const anecdotes = this.props.anecdotes
     const handleVote = (anecdote) => {
-      this.props.store.dispatch(anecdoteVoter(anecdote.id))
-      this.props.store.dispatch(notificationAdder('Voted "' + anecdote.content + '"'))
+      this.props.anecdoteVoter(anecdote.id)
+      this.props.notificationAdder('Voted "' + anecdote.content + '"')
       setTimeout(() => {
-        this.props.store.dispatch(notificationResetter())
+        this.props.notificationResetter()
       }, 5000)
     }
     return (
       <div>
         <h2>Anecdotes</h2>
         {anecdotes.sort((a, b) => b.votes - a.votes)
-          .filter(anecdote => anecdote.content.match(new RegExp(this.props.store.getState().filter, 'i')))
+          .filter(anecdote => anecdote.content.match(new RegExp(this.props.filter, 'i')))
           .map(anecdote =>
             <div key={anecdote.id}>
               <div>
@@ -37,4 +38,16 @@ class AnecdoteList extends React.Component {
   }
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  return {
+    filter: state.filter,
+    anecdotes: state.anecdotes
+  }
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps,
+  { anecdoteVoter, notificationAdder, notificationResetter }
+)(AnecdoteList)
+
+export default ConnectedAnecdoteList
